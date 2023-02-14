cab77df291bd8f251ea1b74a9b4c401efeda3585
eric <33834009+ericrcwu001@users.noreply.github.com>
2023-02-14 06:39:04 +0000

    DONE

--- src/DogMaker.java
+++ src/DogMaker.java
@@ -1,8 +1,10 @@
-public class DogMaker {
-  
+class DogMethod {
   public static void main(String[] args) {
-    
-    
+    Dog dog = new Dog(2, "Jill", "black and tan");
+    dog.setName("Jessie");
+    System.out.println(dog.getAge());
+    int age = dog.birthday();
+    System.out.println(age);
+    System.out.println(dog);
   }
-  
 }
